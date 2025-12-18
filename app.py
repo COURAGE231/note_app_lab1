d6f300b19bd03555b9c0bab12600e10db4d4c92c
COURAGE231 <zerkon190@yandex.ru>
2025-12-18 13:15:07 +0300

    Disable debug by default, control via FLASK_DEBUG env var

--- app.py
+++ app.py
@@ -243,9 +243,12 @@ def add_security_headers(response):
 # ---------------------------
 # ТОЧКА ВХОДА
 # ---------------------------
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    # Небезопасно: отладка включена и слушаем все интерфейсы
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # Безопасный вариант:
+    # по умолчанию debug выключен, включается только через переменную окружения FLASK_DEBUG=1
+    debug_mode = os.getenv("FLASK_DEBUG", "0") == "1"
+    app.run(host="127.0.0.1", port=5000, debug=debug_mode)
